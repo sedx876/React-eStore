dbd32c731117fc14a08dfa58d95faa4203adb6f8
sedx876 <swatkins876@gmail.com>
2020-08-13 11:11:56 -0400

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Default from './components/Default';
+import Cart from './components/Cart/Cart';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <h1>React eStore</h1>
+        <Navbar />
+        <ProductList />
+        <Details />
+        <Default />
+        <Cart />
       </React.Fragment>
     )
   }
