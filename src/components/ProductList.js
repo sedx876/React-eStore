243120d4bea976e89790611e3010162f00722afd
sedx876 <swatkins876@gmail.com>
2020-08-13 13:00:07 -0400

    context.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Product from './Product';
 import Title from "./Title";
+import { storeProducts } from "../data";
 
 export default class ProductList extends Component {
   state={
-    products:[]
-  }
+    products: storeProducts
+  };
 
   render() {
     return (
