1c7fd655d370c0e4186b77662ac2f484f5bf324d
sedx876 <swatkins876@gmail.com>
2020-08-13 12:24:46 -0400

    button.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../logo.svg';
 import styled from 'styled-components';
+import {ButtonContainer} from './Button';
 
 export default class Navbar extends Component {
   render() {
@@ -19,10 +20,10 @@ export default class Navbar extends Component {
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <button>
-            <i className="fas fa-cart-plus"></i>
+          <ButtonContainer>
+            <span className="mr-2"><i className="fas fa-cart-plus"></i></span>
             My Cart
-          </button>
+          </ButtonContainer>
         </Link>
       </nav>
     )
@@ -30,4 +31,20 @@ export default class Navbar extends Component {
 }
 
 
-const ButtonContainer = styled.button
+// const ButtonContainer = styled.button`
+//  text-transform: capitalize;
+//  font - size: 1.4rem;
+//  background: transparent;
+//  border: 0.05rem solid var(--lightBlue);
+//  border - radius: 0.5rem;
+//  padding: 0.2rem 0.5rem;
+//  outline - color: red;
+//  cursor: pointer;
+//  display: inline - block;
+//  margin: 0.2rem 0.5rem 0.2rem 0;
+//  transition: all 0.5s ease -in -out;
+//    &: hover {
+//    background: var(--lightBlue);
+//    color: var(--mainBlue);
+//  }
+//  `;
