8da8dd1b1d1977cd50aa310a4b6790eb35183c8f
sedx876 <swatkins876@gmail.com>
2020-08-13 12:33:42 -0400

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import {ButtonContainer} from './Button';
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <Nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
         {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
         Creative Commons (Attribution 3.0 Unported);
         https://www.iconfinder.com/Makoto_msk */}
@@ -25,26 +25,19 @@ export default class Navbar extends Component {
             My Cart
           </ButtonContainer>
         </Link>
-      </nav>
+      </Nav>
     )
   }
 }
 
-
-// const ButtonContainer = styled.button`
-//  text-transform: capitalize;
-//  font - size: 1.4rem;
-//  background: transparent;
-//  border: 0.05rem solid var(--lightBlue);
-//  border - radius: 0.5rem;
-//  padding: 0.2rem 0.5rem;
-//  outline - color: red;
-//  cursor: pointer;
-//  display: inline - block;
-//  margin: 0.2rem 0.5rem 0.2rem 0;
-//  transition: all 0.5s ease -in -out;
-//    &: hover {
-//    background: var(--lightBlue);
-//    color: var(--mainBlue);
-//  }
-//  `;
+const Nav = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size:1.3rem;
+    text-transform:capitalize;
+  }
+  @media (max-width: 576px) {
+    .navbar-nav {
+      flex-direction: row !important;
+`;
